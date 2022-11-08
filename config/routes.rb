4fe281430d97031879900df332a01668df74bf2e
shahira-sadat <shahira.sadat1@gmail.com>
2022-11-08 14:43:27 +0430

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :foods
-  # this root will be used later on
-  # root "users#index"
+  get 'foods/index'
+  resources :foods, except: :update
   resources :users do
     resources :recipe_foods, except: :update
     resources :recipes, except: :update
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
