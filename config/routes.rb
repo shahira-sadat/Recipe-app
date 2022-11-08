4fb19c0885c718491bd4964078401a3ea0539f85
Aiziji <aletv1501@gmail.com>
2022-11-08 11:29:51 -0400

    Initialize devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_scope :user do
+    get 'logout', to: 'devise/sessions#destroy'
+    get 'confirmation_email', to: 'devise/confirmations#show'
+  end
+  root 'foods#index'
   get 'foods/index'
   resources :foods, except: :update
   resources :users do
