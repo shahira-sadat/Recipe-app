9f4d9d638bd5582a051107240d89f0bdb5f4e4d9
shahira-sadat <shahira.sadat1@gmail.com>
2022-11-09 04:11:51 +0430

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  devise_scope :user do
-    get 'logout', to: 'devise/sessions#destroy'
-    get 'confirmation_email', to: 'devise/confirmations#show'
-  end
-  root 'foods#index'
   get 'foods/index'
-  resources :foods, except: :update
-  resources :users do
-    resources :recipe_foods, except: :update
-    resources :recipes, except: :update
-  end
+  resources :foods, only:[:index,:new,:create,:destroy]
+  get 'users/index'
+  get 'users/show'
+  devise_for :users
+  root "users#index"
 end
