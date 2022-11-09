565e9abd7e8c9b60fff8bb437ebb43c22b525301
shahira-sadat <shahira.sadat1@gmail.com>
2022-11-09 13:46:48 +0430

    Add logout route

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   get 'users/index'
   get 'users/show'
   devise_for :users
+  devise_scope :user do
+   get 'logout', to: 'devise/sessions#destroy' 
+  end
   root "users#index"
 end
