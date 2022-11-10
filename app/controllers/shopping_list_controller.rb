e58fa25b2a108ca6db58d173ef184fb8adfd64f2
Aiziji <aletv1501@gmail.com>
2022-11-10 18:01:02 -0400

    Add basic variables for shopping_list_controller

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,6 +1,16 @@
 class ShoppingListController < ApplicationController
   def index
     @user = User.find(current_user.id) 
-    @foods = @user.foods
+    @recipe = @user.recipes.find(params[:recipe])
+    @missing_food = []
+    @total_price = 0
+    @total_missing_food = 0
+    @recipe.recipe_foods.each do |food|
+      next if (Food.find(food.food_id).quantity).nil?
+      next if (food.quantity) <= (Food.find(food.food_id).quantity)
+      @total_value += (food.quantity - Food.find(food.food_id).quantity) * food.food.price
+      @total_missing_food += 1
+      @missing_food << food
+    end
   end
 end
