4350dcab2c8840a4df68afb4ce3565975d84e5e5
shahira-sadat <shahira.sadat1@gmail.com>
2022-11-10 17:34:25 +0430

    Add test for recipe

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
-
 RSpec.describe Recipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @user = User.new(name: 'ahmad', email: 'ahmad@gmail.com', password: 123456, password_confirmation: 123456)
+    @user.skip_confirmation!
+    @user.save
+  end
+  it 'is valid' do
+    recipe = Recipe.new(name: 'Chicken wings', preparation_time: '1 hours', cooking_time: '2 hours',
+                            description: 'Test', public: false, user_id: @user.id)
+    expect(recipe).to be_valid
+  end
+  it 'should not be valid without a name' do
+    recipe = Recipe.new(name: nil, preparation_time: '1 hours', cooking_time: '2 hours',
+                            description: 'Test', public: false, user_id: @user.id)
+    expect(recipe).to_not be_valid
+  end
+  it 'should not be valid without cooking time' do
+    recipe = Recipe.new(name: 'Chicken wings', preparation_time: '1 hours', cooking_time: nil,
+                            description: 'Test', public: false, user_id: @user.id)
+    expect(recipe).to_not be_valid
+  end
+  it 'should not be valid without preparation time' do
+    recipe = Recipe.new(name: 'Chicken wings', preparation_time: nil, cooking_time: '2 hours',
+                            description: 'Test', public: false, user_id: @user.id)
+    expect(recipe).to_not be_valid
+  end
+  it 'should not be valid without a description' do
+    recipe = Recipe.new(name: 'Chicken wings', preparation_time: '1 hours', cooking_time: '2 hours',
+                            description: nil, public: false, user_id: @user.id)
+    expect(recipe).to_not be_valid
+  end
 end
