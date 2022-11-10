73c3f45f8a2e31c1414a57c58f27a2e6e02ad3cc
shahira-sadat <shahira.sadat1@gmail.com>
2022-11-10 17:34:00 +0430

    Add test for recipe-foods

--- spec/models/recipe_food_spec.rb
+++ spec/models/recipe_food_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe RecipeFood, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @user = User.create(name: 'ahmad', email: 'ahmad@gmail.com', password: 123456, password_confirmation: 123456)
+    @user.skip_confirmation!
+    @recipe = Recipe.create(name: 'Chicken wings', preparation_time: '1 hours', cooking_time: '2 hours',
+                            description: 'Test', public: false, user_id: @user.id)
+    @food = Food.create(name: 'Cake', measurement_unit: 'kilogram', price: 10, user_id: @user.id)
+  end
+  it 'is valid' do
+    recipe_food = RecipeFood.new(recipe_id: @recipe.id, food_id: @food.id, quantity: 2)
+    expect(recipe_food).to be_valid
+  end
+  it 'should not be valid without quantity' do
+    recipe_food = RecipeFood.new(recipe_id: @recipe.id, food_id: @food.id, quantity: nil)
+    expect(recipe_food).to_not be_valid
+  end
 end
