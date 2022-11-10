02a13ca526dadec3decc720862d1e8dcfc8a9672
shahira-sadat <shahira.sadat1@gmail.com>
2022-11-10 17:33:34 +0430

    Add test for foods

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @user = User.create(name: 'ahmad', email: 'ahmad@gmail.com', password: 123456, password_confirmation: 123456)
+    @user.skip_confirmation!
+    @food = Food.create(name: 'Cake', measurement_unit: 'kilogram', price: 10, user_id: @user.id)
+  end
+
+  it 'name should not be blank' do
+    subject.name = 'Cake'
+    expect(subject.name).to eq 'Cake'
+  end
+  it 'measurement_unit field should not be blank' do
+    subject.measurement_unit = 'kilograms'
+    expect(subject.measurement_unit).to eq 'kilograms'
+  end
+
+  it 'price must be an integer greater than or equal to 0' do
+    subject.price = 3
+    expect(subject.price).to be >= 0
+  end
 end
